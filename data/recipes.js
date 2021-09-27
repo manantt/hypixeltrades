7e733de46d0d1f694c5f56428f49b6ccfd975899
manantt <manantt@hotmail.com>
2021-09-27 14:29:00 +0200

    Initial commit

---
+++ data/recipes.js
@@ -0,0 +1,109 @@
+const recipes = [
+  /* FARMING */
+  {
+    "WHEAT": [
+      {"HAY_BLOCK": [{"key": "WHEAT", "quantity": 9}]},
+      {"ENCHANTED_HAY_BLOCK": [{"key": "WHEAT", "quantity": 1296}]},
+      {"TIGHTLY_TIED_HAY_BALE": [{"key": "WHEAT", "quantity": 1296*144}]},
+    ]
+  },
+  {
+    "CARROT": [
+      {"ENCHANTED_CARROT": [{"key": "CARROT_ITEM", "quantity": 160}]},
+      /* {"ENCHANTED_GOLDEN_CARROT": [
+        {"key": "CARROT_ITEM", "quantity": 20512},
+        {"key": "GOLD_INGOT", "quantity": 28.45}
+      ]}, */
+    ]
+  },
+      {
+    "POTATO": [
+      {"ENCHANTED_POTATO": [{"key": "POTATO_ITEM", "quantity": 160}]},
+      {"ENCHANTED_BAKED_POTATO": [{"key": "POTATO_ITEM", "quantity": 160*160}]},
+    ]
+  },
+  {
+    "PUMPKIN": [
+      {"ENCHANTED_PUMPKIN": [{"key": "PUMPKIN", "quantity": 160}]},
+    ]
+  },
+  {
+    "MELON": [
+      {"ENCHANTED_MELON": [{"key": "MELON", "quantity": 160}]},
+      {"ENCHANTED_MELON_BLOCK": [{"key": "MELON", "quantity": 160*160}]},
+    ]
+  },
+  {
+    "SEEDS": [
+      {"ENCHANTED_SEEDS": [{"key": "SEEDS", "quantity": 160}]},
+    ]
+  },
+  {
+    "MUSHROOMS": [
+      {"ENCHANTED_BROWN_MUSHROOM": [{"key": "BROWN_MUSHROOM", "quantity": 160}]},
+      {"ENCHANTED_RED_MUSHROOM": [{"key": "RED_MUSHROOM", "quantity": 160}]},
+    ]
+  },
+  {
+    "CACTUS": [
+      {"ENCHANTED_CACTUS_GREEN": [{"key": "CACTUS", "quantity": 160}]},
+      {"ENCHANTED_CACTUS": [{"key": "CACTUS", "quantity": 160*160}]},
+    ]
+  },
+  /* MINING */
+  {
+    "COBBLESTONE": [
+      {"ENCHANTED_COBBLESTONE": [{"key": "COBBLESTONE", "quantity": 160}]},
+    ]
+  },
+  {
+    "COAL": [
+      {"ENCHANTED_COAL": [{"key": "COAL", "quantity": 160}]},
+      {"ENCHANTED_COAL_BLOCK": [{"key": "COAL", "quantity": 160*160}]},
+    ]
+  },
+  {
+    "OBSIDIAN": [
+      {"ENCHANTED_OBSIDIAN": [{"key": "OBSIDIAN", "quantity": 160}]},
+    ]
+  },
+  {
+    "DIAMOND": [
+      {"ENCHANTED_DIAMOND": [{"key": "DIAMOND", "quantity": 160}]},
+    ]
+  },
+  {
+    "DIAMOND": [
+      {"ENCHANTED_DIAMOND_BLOCK": [{"key": "DIAMOND", "quantity": 25600}]},
+    ]
+  },
+  {
+    "GLOWSTONE": [
+      {"ENCHANTED_GLOWSTONE_DUST": [{"key": "GLOWSTONE_DUST", "quantity": 160}]},
+    ]
+  },
+  /* COMBAT */
+  {
+    "BONE": [
+      {"ENCHANTED_BONE": [{"key": "BONE", "quantity": 160}]},
+    ]
+  },
+  {
+    "STRING": [
+      {"ENCHANTED_STRING": [{"key": "STRING", "quantity": 160}]},
+    ]
+  },
+  /* SLAYER */
+  {
+    "VOID": [
+      {"NULL_OVOID": [{"key": "NULL_SPHERE", "quantity": 160}]},
+      {"SOULFLOW": [{"key": "RAW_SOULFLOW", "quantity": 160}]},
+    ]
+  },
+  /* FISHING */
+  {
+    "CLAY": [
+      {"ENCHANTED_CLAY_BALL": [{"key": "CLAY_BALL", "quantity": 160}]},
+    ]
+  },
+];
